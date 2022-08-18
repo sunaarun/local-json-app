c46b4cb0b61901c3ac5bb1272e99dda956891739
Sana'a Al-ahdal <36349126+sunaarun@users.noreply.github.com>
2022-08-18 19:27:51 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
+      debugShowCheckedModeBanner: false,
+      theme:ThemeData(
+        primarySwatch: Colors.indigo
       ),
-      home: jsonPage()//MyHomePage(title: 'Flutter Demo Home Page'),
+      home: jsonPage()
     );
   }
 }
